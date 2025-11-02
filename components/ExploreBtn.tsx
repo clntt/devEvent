b837ed64fff885646d3e6ceb1ea15a52c36fb73f
clnt <c.connery14@gmail.com>
2025-11-02 09:44:44 +0100

    eventcard, homepage, navbar etc implementation

---
+++ components/ExploreBtn.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+const ExploreBtn = () => {
+  return (
+    <button
+      type="button"
+      id="explore-btn"
+      className="mt-7 mx-auto"
+      onClick={() => console.log("clicked")}
+    >
+      <Link href="#Events">
+        Explore Events
+        <Image
+          src="/icons/arrow-down.svg"
+          alt="arrow-down"
+          width={24}
+          height={24}
+        />
+      </Link>
+    </button>
+  );
+};
+
+export default ExploreBtn;

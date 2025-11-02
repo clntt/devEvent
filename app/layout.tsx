ac29ab9053c32316ea8684c06f9ac2538fbdf2f1
clnt <c.connery14@gmail.com>
2025-11-02 08:17:47 +0100

    first creation commit

---
+++ app/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
+import "./globals.css";
+import LightRays from "../components/LightRays";
+
+const schibstedGrotesk = Schibsted_Grotesk({
+  variable: "--font-schibsted-grotesk",
+  subsets: ["latin"],
+});
+
+const martianMono = Martian_Mono({
+  variable: "--font-martian-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DevEvent",
+  description: "The Hub for every Dev Event You Mustn't Miss.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
+      >
+        <div style={{ width: "100%", height: "600px", position: "relative" }}>
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#00ffff"
+            raysSpeed={1.5}
+            lightSpread={0.8}
+            rayLength={1.2}
+            followMouse={true}
+            mouseInfluence={0.1}
+            noiseAmount={0.1}
+            distortion={0.05}
+            className="custom-rays"
+          />
+        </div>
+        {children}
+      </body>
+    </html>
+  );
+}
